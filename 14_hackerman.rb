4b87de85521adc524b4df5d59247edb368ff22fc
m0n4d1 <m0n4d1000@gmail.com>
2019-03-16 13:58:14 +1000

    hackerman challenge updated

--- 14_hackerman.rb
+++ 14_hackerman.rb
@@ -22,17 +22,30 @@ def get_input
   gets.strip.chomp.split(" ")
 end
 
-def hide_symmetry( first_input = get_input, second_input = get_input )
-  sym_diff = (first_input - second_input) + (second_input - first_input)
-  sym = (first_input | second_input) - sym_diff
-  return sym_diff.map { |x| x.downcase } + sym.map { "HIDDEN" } 
+
+def hide_intersection( a = get_input, b = get_input )
+  
+  #PARAMETERS
+  #---------------
+  #Gets 2 user input strings a and b splitting each into an array of words (or optionally you may provide 2 arrays of words). 
+  
+  #BODY (from ltr)
+  #---------------
+  #Subtract the intersection of sets a and b from the union of sets a and b to get the disjunctive union of sets a and b.
+  #NOTE: the disjunctive union of sets a and b will be the set of words persent in either one of the user input strings but not the other.
+  #Map over the disjunctive union of sets a and b calling downcase on each element.
+  #Take the intersection of sets a and b mapping over each element replacing it with the string "HIDDEN" then add this modified intersection to the modified disjunctive union.
+  #NOTE: This modified interesection will be the set of words present in both user input strings replaced with "HIDDEN".
+  #Modified union of sets a and b is returned.
+  
+  return ((a | b) - (a & b)).map { |x| x.downcase } + (a & b).map { "HIDDEN" }
+
 end
 
-p hide_symmetry()
-
-# testing
-# arr1 = ["One", "two", "three"]
-# arr2 = ["two", "three", "Four", "five"]
+p hide_symmetry
+# tests
+# arr1 = ["one","two", "three"]
+# arr2 = ["three", "four", "five"]
 # p hide_symmetry(arr1,arr2)
 
 
